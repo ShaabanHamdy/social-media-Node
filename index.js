dd7f081792543f3c48481ec30994cbb36c3c94a3
Shaaban <shabanhamdy94@gmail.com>
2023-06-26 19:11:27 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import express from 'express'
 import initApp from './Src/Utils/initApp.js'
-config({ path: './DB/Config/secret.env' })
+config({ path: './secret.env' })
 const app = express()
 
 
